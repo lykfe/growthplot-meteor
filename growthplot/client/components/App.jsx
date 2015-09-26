4f9190d9b69ff9661153abe9c9b0f02576e63637
Akiff Manji <akiff.manji@gmail.com>
2015-09-25 22:36:13 -0700

    Added react package and set up preliminary application structure

---
+++ growthplot/client/components/App.jsx
@@ -0,0 +1,15 @@
+//Housing for entire application
+App = React.createClass({
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>growthplot</h1>
+        </header>
+
+        App will be housed here
+
+      </div>
+    );
+  }
+});
