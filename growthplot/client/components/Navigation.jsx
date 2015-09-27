21fc1d3987e3a0b7582a01eeedfd66279c31025a
Eric Kim <bosung90@gmail.com>
2015-09-27 03:35:27 -0700

    Added navigation, but I am not sure why the styling is so off. There must be some other css that got included when we ran in Django that is missing here

--- growthplot/client/components/Navigation.jsx
+++ growthplot/client/components/Navigation.jsx
@@ -1,12 +1,42 @@
 Navigation = React.createClass({
   render() {
     return (
-      <div className="container">
-        <header>
-          <h1>Navigation Goes here</h1>
-        </header>
+    	<div id="navigation-bar">
+         <div className="navbar navbar-inverse navbar-fixed-top" role="navigation" id="slide-nav">
+		  <div className="container">
+		   <div className="navbar-header">
+		    <a className="navbar-toggle"> 
+		      <span className="sr-only">Toggle navigation</span>
+		      <span className="icon-bar"></span>
+		      <span className="icon-bar"></span>
+		      <span className="icon-bar"></span>
+		     </a>
+		   <div class ="img-container">
+		    <a className="navbar-brand" href="#"><img src="/img/assets/LogoWhite(NavBar).png" /></a>
+		  </div>
+		   </div>
+		   <div id="slidemenu">
+		    <ul className="nav navbar-nav">
+		     <li> <div className="img-container2"><img src="/img/assets/LogoWhite(NavBar).png" /></div></li>
+		     <li><a href="/">Home</a></li>
+		     <li><a href="/about">About</a></li>
 
-      </div>
+		     {/*{% if not user.is_authenticated %}*/}
+		     <li><a href="/register">Register</a></li>
+		     {/*{% endif %}*/}
+
+		   	 {/*{% if user.is_authenticated %}*/}
+		   	 <li><a href="/child">Add Child</a></li>
+		   	 <li><a href="/profile">Growth Charts</a></li>
+		     <li><a href="/logout">Logout</a></li>
+		     {/*{% endif %}*/}
+
+		    </ul>
+		          
+		   </div>
+		  </div>
+		 </div>
+		</div>
     );
   }
 });
